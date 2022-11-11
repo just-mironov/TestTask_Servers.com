a9cee8c0de4dd3d1a7dd6a338128cf64ecbff8c0
Aleksandr Mironov <alexandrmironovo@gmail.com>
2022-11-11 09:54:54 +0200

    Оригинальный скрипт

--- Original.ps1
+++ Original.ps1
@@ -3,12 +3,13 @@ $Running = $true
 $a = Get-WmiObject win32_service | where {$_.Name -match 'MyAwesomeS*'}
 Add-Content -Path "C:\Windows\Temp\MyLog-$((Get-Date).Year)-$((Get-Date).Month)-$((Get-Date).Day).log"`
     -Value "[$((Get-Date).TimeOfDay)] Found servises"
+    
 for ($i=1;$i -lt $a.Count; $i++) {
     if ($a[$i].State -eq 'Stopped') {
         if ($a[$i].StartMode -eq "Automatic") {
             Get-Service $a[$i].Name | Start-Service
             Add-Content -Path "C:\Windows\Temp\MyLog-$((Get-Date).Year)-$((Get-Date).Month)-$((Get-Date).Day).log"`
-                -Value "[$((Get-Date).TimeOfDay)] Started servise $($a[$i].Name) (Автомат)"    
+                -Value "[$((Get-Date).TimeOfDay)] Started servise $($a[$i].Name) (Автомат)"
         } elseif ($a[$i].StartMode -eq "Manual") {
             Get-Service $a[$i].Name | Start-Service
             Add-Content -Path "C:\Windows\Temp\MyLog-$((Get-Date).Year)-$((Get-Date).Month)-$((Get-Date).Day).log"`
